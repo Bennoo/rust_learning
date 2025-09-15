c7f11401823bb80747ba0968e1d44c0525df5883
BEN <toussaint.benoit@gmail.com>
2025-09-15 15:42:08 +0000

    Reorder imports and improve user feedback in number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
 use std::cmp::Ordering;
+use std::io;
 
 use rand::Rng;
 
@@ -7,8 +7,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -19,15 +17,12 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guessed: {guess}");
-
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
+            Err(_) => continue,
         };
+
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
